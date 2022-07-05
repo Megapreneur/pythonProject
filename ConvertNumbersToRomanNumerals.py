6c2e5b8c5fe74ffabbcfdeac05a1617ab74128bb
Megapreneur <ademolamegbabi@gmail.com>
2022-07-05 23:56:12 +0100

    python exercises

--- ConvertNumbersToRomanNumerals.py
+++ ConvertNumbersToRomanNumerals.py
@@ -1,3 +1,15 @@
-thousands = {"", "M", "MM", "MMM"}
-hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
-tens = {"", "X", "XX", "XXX", "XL", ""}
+
+def convertNumberToRoman(number):
+
+    thousands = ["", "M", "MM", "MMM"]
+    hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    units = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+    result = thousands[number / 1000] + hundreds[(number % 1000) / 100] + tens[(number % 100) / 10] + units[number % 10]
+
+    return result
+
+
+
+
