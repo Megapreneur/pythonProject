f36ff35463a8b1ac5ceac42231831c14d8653771
Megapreneur <ademolamegbabi@gmail.com>
2022-07-11 12:22:59 +0100

    python exercises

---
+++ ww/multiple_return.py
@@ -0,0 +1,6 @@
+
+def adder_subtractor(num1, num2):
+    return num1 + num2, num1 - num2, num1 * num2
+
+
+print(adder_subtractor(3, 2))
--- ww/tuple_literal.py
+++ ww/tuple_literal.py
@@ -3,4 +3,13 @@ vowels = ("a", "e", "i", "o", "u")
 for vowel in vowels:
     print(vowel.upper())
 
+
+name, age, occupation = "David", 34, "Programmer"
+print(name)
+print(age)
+print(occupation)
+
 print(my_list[2:5])
+
+question = "o" in vowels
+print(question)
