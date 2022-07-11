5db21de8a4bdd65f456a893d03504196eb0191e7
Megapreneur <ademolamegbabi@gmail.com>
2022-07-11 13:14:19 +0100

    python exercises

---
+++ TupleExercises/excercise_1.py
@@ -0,0 +1,12 @@
+cardinal_numbers = ("first", "second", "third")
+
+print(type(cardinal_numbers))
+print(cardinal_numbers[1])
+
+for number in cardinal_numbers:
+    print(number)
+
+position1, position2, position3 = "first", "second", "third"
+print(position3)
+print(position1)
+print(position2)
