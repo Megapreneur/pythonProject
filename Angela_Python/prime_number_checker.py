f68f036095174405aec34908e39ea0433f586be9
Megapreneur <ademolamegbabi@gmail.com>
2022-07-25 01:20:20 +0100

    python exercises

--- Angela_Python/prime_number_checker.py
+++ Angela_Python/prime_number_checker.py
@@ -1,9 +1,9 @@
 def prime_checker(number):
     for i in range(2, number):
         if number % i == 0:
-            return "it's a prime number"
-        else:
             return "it is not a prime number"
+        else:
+            return "it's a prime number"
 
 
 n = int(input("Check this number: "))
