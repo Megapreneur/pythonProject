6a65b0647c2a00504b53cf34617534fb00551c6c
Megapreneur <ademolamegbabi@gmail.com>
2022-07-25 08:59:28 +0100

    python exercises

--- Angela_Python/prime_number_checker.py
+++ Angela_Python/prime_number_checker.py
@@ -1,6 +1,6 @@
 def prime_checker(number):
     is_prime = True
-    for i in range(2, number-1 ):
+    for i in range(2, number-1):
         if number % i == 0:
             is_prime = False
     if is_prime:
