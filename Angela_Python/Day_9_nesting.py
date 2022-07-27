b5d88e2856e6de168871417f04e222e6d5278f79
Megapreneur <ademolamegbabi@gmail.com>
2022-07-28 00:48:29 +0100

    python exercises

---
+++ Angela_Python/Day_9_nesting.py
@@ -0,0 +1,27 @@
+# Nesting
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Hungary": "Budapest",
+    "Nigeria": "Abuja",
+}
+
+# Nesting a List in a Dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+    "Nigeria": ["Abuja", "Lagos", "Kebbi"],
+}
+
+# Nesting Dictionary in a Dictionary
+
+travel_diary = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 5},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 7},
+    "Nigeria": {"cities_visited": ["Abuja", "Lagos", "Kebbi"], "total_visits": 28},
+}
+
+# Nesting a dictionary in a List.
+
