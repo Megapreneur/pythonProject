69c40be4448224f71ed6d734d8a86330f72455cf
Megapreneur <ademolamegbabi@gmail.com>
2022-07-27 22:35:37 +0100

    python exercises

---
+++ Angela_Python/grading_program.py
@@ -0,0 +1,16 @@
+student_grades = {}
+
+student_scores = {"Harry": 81, "Ron": 78, "Hermione": 99, "Draco": 62, "Neville": 74}
+
+for key in student_scores:
+    score = student_scores[key]
+    if score > 90:
+        student_grades[key] = "Outstanding"
+    elif score > 80:
+        student_grades[key] = "Exceeds Expectation"
+    elif score > 70:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+print(student_grades)
