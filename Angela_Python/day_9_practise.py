bb11c7af898c095377b089826ad7a601cd2c3437
Megapreneur <ademolamegbabi@gmail.com>
2022-07-27 21:09:29 +0100

    python exercises

---
+++ Angela_Python/day_9_practise.py
@@ -0,0 +1,7 @@
+programming_dict = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again.",
+}
+
+print(programming_dict["Bug"])
