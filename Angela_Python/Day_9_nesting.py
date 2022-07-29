461a3e6064350880d320910a2bbc14068a56ff61
Megapreneur <ademolamegbabi@gmail.com>
2022-07-29 05:26:54 +0100

    python exercises

--- Angela_Python/Day_9_nesting.py
+++ Angela_Python/Day_9_nesting.py
@@ -25,3 +25,20 @@ travel_diary = {
 
 # Nesting a dictionary in a List.
 
+travel_diary2 = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 5
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 7
+    },
+    {
+        "country": "Nigeria",
+        "cities_visited": ["Abuja", "Lagos", "Imo"],
+        "total_visits": 28
+    }, 
+]
