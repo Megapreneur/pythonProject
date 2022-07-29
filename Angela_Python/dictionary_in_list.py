4caa67f0fa585f0ec4ec853421189c35907fbae0
Megapreneur <ademolamegbabi@gmail.com>
2022-07-29 12:15:52 +0100

    python exercises

---
+++ Angela_Python/dictionary_in_list.py
@@ -0,0 +1,23 @@
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 5
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 7
+    },
+    {
+        "country": "Nigeria",
+        "cities_visited": ["Abuja", "Lagos", "Imo"],
+        "total_visits": 28
+    },
+]
+def add_new_country(country, cities_visited, total_visits):
+
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+
+print(travel_log)
