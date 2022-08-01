ae3fa61c185a510ab902a5deadd66a0f820998fb
Megapreneur <ademolamegbabi@gmail.com>
2022-08-02 00:18:07 +0100

    python exercises

--- Angela_Python/secret_auction_program.py
+++ Angela_Python/secret_auction_program.py
@@ -24,15 +24,17 @@
 bids = {}
 bidding_finished = False
 
+
 def find_highest_bidder(bidding_record):
     highest_bid = 0
-    winner =""
+    winner = ""
     for bidder in bidding_record:
         bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
     print(f"The winner is {winner} with a bid of ₦{highest_bid}")
+
 
 while not bidding_finished:
     name = input("What is your name? ")
