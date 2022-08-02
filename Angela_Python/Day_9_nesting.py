9c8c4319f7e7e426c604f0ab3185e0f5feec8894
Megapreneur <ademolamegbabi@gmail.com>
2022-08-02 21:55:16 +0100

    python exercises

--- Angela_Python/Day_9_nesting.py
+++ Angela_Python/Day_9_nesting.py
@@ -42,3 +42,9 @@ travel_diary2 = [
         "total_visits": 28
     },
 ]
+
+#
+# number = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}
+#
+# result = [x for x in number if x % 2 == 0 and x % 3 == 0]
+# print(result)
---
+++ Angela_Python/day_ten.py
@@ -0,0 +1,11 @@
+# Functions with outputs
+
+def format_name(f_name, l_name):
+    first = f_name.title()
+    last = l_name.title()
+
+    return first + " " + last
+
+
+print(format_name("asake", "toYin"))
+
