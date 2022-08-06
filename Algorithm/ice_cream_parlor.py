dd799c6291f873729d7d5ab9ae24d2df3494e959
Megapreneur <ademolamegbabi@gmail.com>
2022-08-06 23:08:32 +0100

    python exercises

---
+++ Algorithm/ice_cream_parlor.py
@@ -0,0 +1,12 @@
+def icecream_parlor(m, arr):
+
+    new_arr = []
+    for x in arr:
+        new_arr.append(x + m)
+
+    print(new_arr)
+
+
+list_n = [1, 2, 3, 4, 5, 6]
+
+icecream_parlor(4, list_n)
