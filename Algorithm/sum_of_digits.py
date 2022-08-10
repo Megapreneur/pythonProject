ca01ea9b01420728af5fda690df9d8ce0135732e
Megapreneur <ademolamegbabi@gmail.com>
2022-08-10 07:45:40 +0100

    python exercises

---
+++ Algorithm/sum_of_digits.py
@@ -0,0 +1,21 @@
+# n = 942
+#
+#
+# def digital_root(n):
+#     num = [x for x in n]
+#     for a in num:
+#         sum(a)
+#     while len(sum(a)) > 1:
+#
+#
+#
+#
+#
+# digital_root(n)
+
+
+names = ["ALex", "Beth", "Caroline", "Eleanor", "Freddie"]
+
+upper_case_name = [name.upper() for name in names if len(name) >= 5]
+
+print(upper_case_name)
